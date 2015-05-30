3c106b70749a7566c7d4ff7571d045bd78426464
Bronislav Milke <mielke@MacBook-Pro-Bronislav.local>
2015-05-31 01:47:05 +0300

    Gulfile.js reboot

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
 var gulp = require('gulp'),
-	connect = require("gulpconnect");
+	connect = require("gulp-connect"),
+    opn = require('opn');
 
 
 //Задачи для gulp-connect
-// Запуск сервера
+
+//Запуск сервера
 gulp.task('connect', function() {
     connect.server({
         root: 'app',
         livereload: true,
         port: 8888
     });
-});
-// Работа с html
+    opn('http:/localhost:8888');
+});
+
+//работа с html
 gulp.task('html', function() {
     gulp.src('./app/*.html')
-        .pipe(connect.reload());
-});
-
-// Работа с css
+        .pipe(connect.reload());
+});
+//работа с css
 gulp.task('css', function() {
     gulp.src('./app/style/*.css')
-        .pipe(connect.reload());
-});
-
-// Работа с js
+        .pipe(connect.reload());
+});
+//работа с js
 gulp.task('js', function() {
     gulp.src('./app/script/*.js')
-        .pipe(connect.reload());
-});
-// Слежка
+        .pipe(connect.reload());
+});
+//слежка
 gulp.task('watch', function() {
-        gulp.watch(['./app/*.html'], ['html']);
-        gulp.watch(['./app/css/*.css'], ['css']);
-        gulp.watch(['./app/js/*.js'], ['js']);
-    }
+    gulp.watch(['.app/*.html'],['html']);
+    gulp.watch(['.app/style/*.css'],['css']);
+    gulp.watch(['.app/script/*.html'],['js']);
+});
 
-// Задача поумолчанию
-gulp.task('default', ['connect', 'watch']);
+//задача по умолчанию
+gulp.task('default', ['connect', 'watch']);
 
